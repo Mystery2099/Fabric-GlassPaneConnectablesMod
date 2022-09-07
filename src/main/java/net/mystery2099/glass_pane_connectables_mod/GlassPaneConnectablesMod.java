fe53847a613c52f53473ffa3deffb189e521737d
Mystery2099 <xxmysteryxxftb@gmail.com>
2022-09-07 15:11:41 -0400

    implemented ModInitializer

--- src/main/java/net/mystery2099/glass_pane_connectables_mod/GlassPaneConnectablesMod.java
+++ src/main/java/net/mystery2099/glass_pane_connectables_mod/GlassPaneConnectablesMod.java
@@ -1,4 +1,11 @@
 package net.mystery2099.glass_pane_connectables_mod;
 
-public class GlassPaneConnectablesMod {
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.impl.registry.sync.FabricRegistryInit;
+
+public class GlassPaneConnectablesMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
 }
