6bb4f400566f3aee84125298864f97200e8118e4
Mystery2099 <xxmysteryxxftb@gmail.com>
2022-09-07 15:19:35 -0400

    added glass_pane_connectables block tagkey

--- src/main/java/net/mystery2099/glass_pane_connectables_mod/GlassPaneConnectablesMod.java
+++ src/main/java/net/mystery2099/glass_pane_connectables_mod/GlassPaneConnectablesMod.java
@@ -2,8 +2,14 @@ package net.mystery2099.glass_pane_connectables_mod;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.impl.registry.sync.FabricRegistryInit;
+import net.minecraft.block.Block;
+import net.minecraft.tag.TagKey;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class GlassPaneConnectablesMod implements ModInitializer {
+    public static final TagKey<Block> GLASS_PANE_CONNECTABLES_TAG_KEY = TagKey.of(Registry.BLOCK_KEY, new Identifier("c", "glass_pane_connectables"));
+
     @Override
     public void onInitialize() {
 
