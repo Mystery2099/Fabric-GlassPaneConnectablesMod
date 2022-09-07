1c65bb38c4e5ff46d5bfa8be577709a24b86655b
Mystery2099 <xxmysteryxxftb@gmail.com>
2022-09-07 15:08:14 -0400

    Initial commit

---
+++ src/main/java/net/mystery2099/glass_pane_connectables_mod/GlassPaneConnectablesMod.java
@@ -0,0 +1,4 @@
+package net.mystery2099.glass_pane_connectables_mod;
+
+public class GlassPaneConnectablesMod {
+}
---
+++ src/main/java/net/mystery2099/glass_pane_connectables_mod/client/GlassPaneConnectablesModClient.java
@@ -0,0 +1,5 @@
+package net.mystery2099.glass_pane_connectables_mod.client;
+
+@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+public class GlassPaneConnectablesModClient {
+}
