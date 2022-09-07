a733e8cc3c4f02c4b52cd16feeea5aac4b5d414c
Mystery2099 <xxmysteryxxftb@gmail.com>
2022-09-07 15:11:51 -0400

    implemented ClientModInitializer

--- src/main/java/net/mystery2099/glass_pane_connectables_mod/client/GlassPaneConnectablesModClient.java
+++ src/main/java/net/mystery2099/glass_pane_connectables_mod/client/GlassPaneConnectablesModClient.java
@@ -1,5 +1,11 @@
 package net.mystery2099.glass_pane_connectables_mod.client;
 
+import net.fabricmc.api.ClientModInitializer;
+
 @net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
-public class GlassPaneConnectablesModClient {
+public class GlassPaneConnectablesModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
 }
